9004dd676dc5636b61d4f6fa9fd4e1b72a23fc6c
NicolasPozzer <pozzernico@gmail.com>
2023-05-02 16:32:35 -0300

    controller Proyecto creado

---
+++ src/main/java/com/portfolio/nico/Controller/CProyecto.java
@@ -0,0 +1,94 @@
+package com.portfolio.nico.Controller;
+
+import com.portfolio.nico.Dto.dtoProyecto;
+import com.portfolio.nico.Entity.Proyecto;
+import com.portfolio.nico.Security.Controller.Mensaje;
+import com.portfolio.nico.Service.SProyecto;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/project")
+@CrossOrigin(origins = {"https://acaVamiWeb", "http://localhost:4200"})
+public class CProyecto {
+
+    @Autowired
+    SProyecto sProyecto;
+
+    @GetMapping("/lista")
+    public ResponseEntity<List<Proyecto>> list() {
+        List<Proyecto> list = sProyecto.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Proyecto> getById(@PathVariable("id") int id) {
+        if (!sProyecto.existsById(id)) {
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        }
+        Proyecto proyecto = sProyecto.getOne(id).get();
+        return new ResponseEntity(proyecto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!sProyecto.existsById(id)) {
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        }
+        sProyecto.delete(id);
+        return new ResponseEntity(new Mensaje("Proyecto eliminado"), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoProyecto dtopro) {
+        if (StringUtils.isBlank(dtopro.getNombreE())) {
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if (sProyecto.existsByNombreE(dtopro.getNombreE())) {
+            return new ResponseEntity(new Mensaje("Ese proyecto existe"), HttpStatus.BAD_REQUEST);
+        }
+
+        Proyecto proyecto = new Proyecto(dtopro.getNombreE(), dtopro.getDescripcionE());
+        sProyecto.save(proyecto);
+
+        return new ResponseEntity(new Mensaje("Proyecto agregado"), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoProyecto dtopro) {
+        //Validamos si existe el ID
+        if (!sProyecto.existsById(id)) {
+            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+        }
+        //Compara nombre de experiencias
+        if (sProyecto.existsByNombreE(dtopro.getNombreE()) && sProyecto.getByNombreE(dtopro.getNombreE()).get().getId() != id) {
+            return new ResponseEntity(new Mensaje("Ese Proyecto ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        //No puede estar vacio
+        if (StringUtils.isBlank(dtopro.getNombreE())) {
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
+        Proyecto proyecto = sProyecto.getOne(id).get();
+        proyecto.setNombreE(dtopro.getNombreE());
+        proyecto.setDescripcionE((dtopro.getDescripcionE()));
+
+        sProyecto.save(proyecto);
+        return new ResponseEntity(new Mensaje("Proyecto actualizado"), HttpStatus.OK);
+
+    }
+}
+
+}
