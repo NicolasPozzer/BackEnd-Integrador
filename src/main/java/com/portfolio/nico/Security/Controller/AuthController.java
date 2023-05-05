a4d5356eba063be83813ab13a92447f2bbb3c6b0
NicolasPozzer <pozzernico@gmail.com>
2023-05-05 16:55:54 -0300

    fix-cors-login

--- src/main/java/com/portfolio/nico/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/nico/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://testweb.com","http://localhost:4200"})
+@CrossOrigin(origins = {"https://backend-nico.onrender.com","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
