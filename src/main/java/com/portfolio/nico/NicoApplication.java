e46df5c68a09e3784e4ea051b2c599e95a68fdee
NicolasPozzer <pozzernico@gmail.com>
2023-09-19 20:40:05 -0300

    test-aws

--- src/main/java/com/portfolio/nico/NicoApplication.java
+++ src/main/java/com/portfolio/nico/NicoApplication.java
@@ -1,24 +1,13 @@
 package com.portfolio.nico;
 
-import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NicoApplication {
 
-    public static void main(String[] args) {
-	SpringApplication app = new SpringApplication(NicoApplication.class);
-	String port = System.getenv("PORT");
-	app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
-	app.run(args);
-    }
-    
-    
-    
-        /*
 	public static void main(String[] args) {
 		SpringApplication.run(NicoApplication.class, args);
-	}*/
-
+	}
+        
 }
