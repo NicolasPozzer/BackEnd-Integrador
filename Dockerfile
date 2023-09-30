5aff250e0542c2a558ebc31d08e129e835e6aa5c
NicolasPozzer <pozzernico@gmail.com>
2023-09-29 21:24:09 -0300

    fix-Dockerfile-default7

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-
-FROM openjdk:17-alpine
-MAINTAINER NICO
-EXPOSE 8080
-COPY target/nico-0.0.1-SNAPSHOT.jar  nico-app.jar
-ENTRYPOINT ["java","-jar","/nico-app.jar"]
--- src/main/java/com/portfolio/nico/NicoApplication.java
+++ src/main/java/com/portfolio/nico/NicoApplication.java
@@ -1,8 +1,10 @@
 package com.portfolio.nico;
 
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
 @SpringBootApplication
 public class NicoApplication {
 
@@ -10,4 +12,14 @@ public class NicoApplication {
 		SpringApplication.run(NicoApplication.class, args);
 	}
         
+}*/
+@SpringBootApplication(proxyBeanMethods = false)
+public class NicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(NicoApplication.class);
+		String port = System.getenv("PORT");
+		app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
+		app.run(args);
+	}
 }
