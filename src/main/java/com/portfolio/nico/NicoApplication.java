26be64de7170506d93025f10e19bc0b99b7387c4
NicolasPozzer <pozzernico@gmail.com>
2023-11-07 14:10:12 -0300

    fix

--- src/main/java/com/portfolio/nico/NicoApplication.java
+++ src/main/java/com/portfolio/nico/NicoApplication.java
@@ -2,10 +2,8 @@ package com.portfolio.nico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class NicoApplication {
 
 	public static void main(String[] args) {
