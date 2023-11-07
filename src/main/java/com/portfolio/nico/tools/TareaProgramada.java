c3d4a2461b9ecebf2b47f0a6cde8d078d1fd6688
NicolasPozzer <pozzernico@gmail.com>
2023-11-07 11:05:40 -0300

    fix url

--- src/main/java/com/portfolio/nico/tools/TareaProgramada.java
+++ src/main/java/com/portfolio/nico/tools/TareaProgramada.java
@@ -15,7 +15,7 @@ public class TareaProgramada {
 
     @Scheduled(fixedRate = 1440000) // 15000 milisegundos = 15 segundos - 1440000 = 24min
     public void realizarPeticionGET() {
-        String url = "https://nicopozzer-backend.fly.dev"; // URL de tu propia aplicación
+        String url = "https://fly-builder-billowing-surf-1589.fly.dev"; // URL de tu propia aplicación
         String respuesta = restTemplate.getForObject(url, String.class);
         // Puedes manejar la respuesta si es necesario
         System.out.println("Respuesta de la petición GET: " + respuesta);
